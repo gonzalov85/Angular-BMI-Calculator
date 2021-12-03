1718f2aa5da4206f592307fa7a7f90d234962877
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-03 20:35:50 -0300

    modules and styles

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  edad = 25;
+  peso = 60;
+  altura = 170;
+  sexo="Masculino";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  cambiarAltura(event: any) {
+     this.altura = event.target.value;
+  }
+
+  masculino(){
+    this.sexo="Masculino"
+  }
+
+  femenino(){
+    this.sexo="Femenino"
+  }
+
+  calcularBMI(){
+
+      this.router.navigate(['/resultado']);
+  }
 }
